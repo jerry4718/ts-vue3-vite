b71356c5269dadff3c72776c2005df0765c7b8c1
jerry4718 <jerry4718@qq.com>
2020-08-21 18:26:25 +0800

    修改async-routes生成代码

--- kits/code-gen/async-routes.js
+++ kits/code-gen/async-routes.js
@@ -7,15 +7,14 @@ const ergodic = require('../ergodic');
 const pagesPath = path.join(__dirname, '../../src/pages');
 const routerPath = path.join(__dirname, '../../src/routes');
 
-const pathPrefix = path.relative(routerPath, pagesPath)
+const pathPrefix = path.relative(routerPath, pagesPath);
 
-const abstractComponents = {
-    '/config/thirdConfigList': '/config/thirdConfigList/:configType'
-}
-
-function getRoutePath(routePath) {
-    return abstractComponents[routePath] || routePath;
-}
+const concurrentMapper = {
+    '/config/thirdConfigList': [
+        '/config/thirdConfigList/AliyunOSS',
+        '/config/thirdConfigList/QiniuCloud',
+    ],
+};
 
 const routesArr = [];
 
@@ -24,21 +23,35 @@ ergodic.dirSync(pagesPath, function(file_name, full_path, dir) {
     const routePath = path.relative(routerPath, dir).replace(pathPrefix, "");
     const componentPath = path.relative(routerPath, full_path);
 
-    const routeName = routePath;
+    console.log(routePath);
 
-    routesArr.push(`    {
-        path: "${getRoutePath(routePath)}",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("${componentPath}")),
-    },`);
-
+    if (concurrentMapper[routePath]) {
+        concurrentMapper[routePath].forEach(s => {
+            routesArr.push(`    // @ts-ignore\n    "${s}": defineAsyncComponent(() => import("${componentPath}")),`);
+        })
+    } else {
+        routesArr.push(`    // @ts-ignore\n    "${routePath}": defineAsyncComponent(() => import("${componentPath}")),`);
+    }
 })
 
 
+/*import {AsyncComponentLoader, AsyncComponentOptions, Component, ComponentPublicInstance} from "@vue/runtime-core";
+
+function convertAnonymousAsyncComponent<T extends Component = {new (): ComponentPublicInstance;}>(source: AsyncComponentLoader<T> | AsyncComponentOptions<T>): T {
+    return defineAsyncComponent(source);
+}*/
+
 const code = `import {defineAsyncComponent} from 'vue';
 
-export const asyncRoutes = [
+const concurrentMapper = {
+
+};
+
+const routesMapper = {
 ${routesArr.join("\n")}
+};
+
+export const asyncRoutes = [
 ];`
 
 fs.writeFileSync(path.join(routerPath, "async-routes.ts"), code);
--- src/routes/async-routes.ts
+++ src/routes/async-routes.ts
@@ -1,69 +1,48 @@
-import {defineAsyncComponent} from 'vue';
+import {
+    AsyncComponentLoader,
+    AsyncComponentOptions,
+    Component,
+    ComponentPublicInstance,
+    defineAsyncComponent
+} from 'vue';
+
+type AnonymousAsyncComponent = new () => ComponentPublicInstance;
+
+function convertAnonymousAsyncComponent<T extends Component = {new (): ComponentPublicInstance;}>(source: AsyncComponentLoader<T> | AsyncComponentOptions<T>): T {
+    return defineAsyncComponent(source);
+}
+
+const routesMapper = {
+    // @ts-ignore
+    "/account/accountList": defineAsyncComponent(() => (import("../pages/account/accountList/accountList.vue"))),
+    // @ts-ignore
+    "/config/localConfigList": defineAsyncComponent(() => import("../pages/config/localConfigList/localConfigList.vue")),
+    // @ts-ignore
+    "/config/thirdConfigList/AliyunOSS": defineAsyncComponent(() => import("../pages/config/thirdConfigList/thirdConfigList.vue")),
+    // @ts-ignore
+    "/config/thirdConfigList/QiniuCloud": defineAsyncComponent(() => import("../pages/config/thirdConfigList/thirdConfigList.vue")),
+    // @ts-ignore
+    "/index/indexMain": defineAsyncComponent(() => import("../pages/index/indexMain/indexMain.vue")),
+    // @ts-ignore
+    "/log/logList": defineAsyncComponent(() => import("../pages/log/logList/logList.vue")),
+    // @ts-ignore
+    "/menu/menuList": defineAsyncComponent(() => import("../pages/menu/menuList/menuList.vue")),
+    // @ts-ignore
+    "/order/normalList": defineAsyncComponent(() => import("../pages/order/normalList/normalList.vue")),
+    // @ts-ignore
+    "/order/overList": defineAsyncComponent(() => import("../pages/order/overList/overList.vue")),
+    // @ts-ignore
+    "/order/verifyList": defineAsyncComponent(() => import("../pages/order/verifyList/verifyList.vue")),
+    // @ts-ignore
+    "/role/roleList": defineAsyncComponent(() => import("../pages/role/roleList/roleList.vue")),
+    // @ts-ignore
+    "/user/blackList": defineAsyncComponent(() => import("../pages/user/blackList/blackList.vue")),
+    // @ts-ignore
+    "/user/businessList": defineAsyncComponent(() => import("../pages/user/businessList/businessList.vue")),
+    // @ts-ignore
+    "/user/normalList": defineAsyncComponent(() => import("../pages/user/normalList/normalList.vue")),
+};
 
 export const asyncRoutes = [
-    {
-        path: "/account/accountList",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/account/accountList/accountList.vue")),
-    },
-    {
-        path: "/config/localConfigList",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/config/localConfigList/localConfigList.vue")),
-    },
-    {
-        path: "/config/thirdConfigList/:configType",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/config/thirdConfigList/thirdConfigList.vue")),
-    },
-    {
-        path: "/index/indexMain",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/index/indexMain/indexMain.vue")),
-    },
-    {
-        path: "/log/logList",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/log/logList/logList.vue")),
-    },
-    {
-        path: "/menu/menuList",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/menu/menuList/menuList.vue")),
-    },
-    {
-        path: "/order/normalList",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/order/normalList/normalList.vue")),
-    },
-    {
-        path: "/order/overList",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/order/overList/overList.vue")),
-    },
-    {
-        path: "/order/verifyList",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/order/verifyList/verifyList.vue")),
-    },
-    {
-        path: "/role/roleList",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/role/roleList/roleList.vue")),
-    },
-    {
-        path: "/user/blackList",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/user/blackList/blackList.vue")),
-    },
-    {
-        path: "/user/businessList",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/user/businessList/businessList.vue")),
-    },
-    {
-        path: "/user/normalList",
-        // @ts-ignore
-        component: defineAsyncComponent(() => import("../pages/user/normalList/normalList.vue")),
-    },
+
 ];
